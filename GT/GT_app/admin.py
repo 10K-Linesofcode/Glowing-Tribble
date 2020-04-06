7f3f6cc492a4a9d8a8853b41cc9e187f6cf2ecdc
10K-LinesOfCode <10k.linesofcoode@gmail.com>
2020-04-06 15:15:15 +0530

    GT-Admin Interface

--- GT/GT_app/admin.py
+++ GT/GT_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from GT_app.models import AccessRecord,Topic,Webpage
+
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
