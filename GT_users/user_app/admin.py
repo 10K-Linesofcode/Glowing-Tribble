ea77ccabf0fb783ab1055e63e9a224e6bbe4c006
10K-LinesOfCode <10k.linesofcoode@gmail.com>
2020-04-07 17:30:20 +0530

    Getting Input and adding in Data base

--- GT_users/user_app/admin.py
+++ GT_users/user_app/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-
 # Register your models here.
-
-
 from user_app.models import User
 admin.site.register(User)
--- GT_users/user_app/views.py
+++ GT_users/user_app/views.py
@@ -5,10 +5,29 @@ from django.shortcuts import render
 
 from user_app.models import User
 
+from user_app.forms import NewUserForm
+
 def index(request):
     return render(request, 'user_app/index.html')
 
+# def users(request):
+#     user_list = User.objects.order_by('first_name')
+#     user_dict = {'users': user_list}
+#     return render(request,'user_app/users.html',context=user_dict)
+
+
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users': user_list}
-    return render(request,'user_app/users.html',context=user_dict)
+
+    form = NewUserForm()
+
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+        else:
+            print(' Error Form Invalid .......')
+
+    return render(request,'user_app/users.html',{'form':form})
