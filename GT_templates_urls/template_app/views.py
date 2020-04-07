d2f3330f940d1608859ae1edf725467a210d6c60
10K-LinesOfCode <10k.linesofcoode@gmail.com>
2020-04-07 20:22:22 +0530

    template tags

--- GT_templates_urls/template_app/views.py
+++ GT_templates_urls/template_app/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request,'templates_app/index.html')
+    context_dict={'text':'Hello world', 'number':1000}
+    return render(request,'templates_app/index.html',context=context_dict)
 
 def others(request):
     return render(request,'templates_app/others.html')
